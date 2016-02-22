99caa5fb7a1355ecd5ed6ce5ced8634ec809e5e7
Alex Nicksay <nicksay@gmail.com>
2016-02-22 15:54:24 -0500

    Script: ensure directories exist

--- install.sh
+++ install.sh
@@ -5,8 +5,13 @@ set -e  # stop on error
 
 cd "$(dirname "$0")"
 
+find . -mindepth 1 -type d -not -path "*.git*" \
+    | cut -c 3- \
+    | tr '\n' '\0' \
+    | xargs -0 -n 1 mkdir -p
+
 rsync -avh --no-perms \
     --exclude ".git/" \
-    --exclude ".DS_Store"
+    --exclude ".DS_Store" \
     --exclude "install.sh" \
     . ~;
