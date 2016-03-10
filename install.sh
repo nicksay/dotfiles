bb9c22c7bcf6e405d1d5e9e864236a135dbd7375
Alex Nicksay <nicksay@gmail.com>
2016-03-10 09:41:17 -0500

    [install] Script: add comments

--- install.sh
+++ install.sh
@@ -3,15 +3,19 @@
 set -e  # stop on error
 
 
+# Change to the script directory.
 cd "$(dirname "$0")"
 
+# Update the files.
 git pull origin
 
+# Make needed directories.
 find . -mindepth 1 -type d -not -path "*.git*" \
     | cut -c 3- \
     | tr '\n' '\0' \
     | xargs -0 -n 1 mkdir -p
 
+# Copy files into place.
 rsync -avh --no-perms \
     --exclude ".git/" \
     --exclude ".DS_Store" \
