17e847e9bead8df90345dca0542ad36cfd6f8c1e
Alex Nicksay <nicksay@gmail.com>
2017-02-15 10:26:53 -0500

    [install] Script: handle spaces in paths and add --help/--dry-run flags

--- install.sh
+++ install.sh
@@ -2,21 +2,72 @@
 
 set -e  # stop on error
 
+# Global variables.
+DRYRUN=0
+
+
+# Functions to handle dry-run versions of commands.
+function _mkdir {
+  # To handle paths piped from find which may have spaces, add the "mkdir"
+  # to each line, echo to the console via tee, then remove the "mkdir" to
+  # be able to pass the paths to mkdir via xargs.
+  sed "s/^/mkdir $@ /" \
+      | tee /dev/tty \
+      | (
+          (( $DRYRUN )) || (
+            sed "s/^mkdir $@ //" \
+                | tr '\n' '\0' \
+                | xargs -0 mkdir $@
+          )
+        )
+}
+
+function _rsync {
+  if (( $DRYRUN )); then
+    echo rsync $@
+    rsync -n $@
+  else
+    rsync $@
+  fi
+}
+
+
+# Process flags.
+while (( $# > 0 )); do
+  if [[ "$1" == "-n" || "$1" == "--dry-run" ]]; then
+    DRYRUN=1
+    echo "
+      Dry run! Commands will not be run, just printed.
+    " | sed 's/^ \{6\}//'
+  elif [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    echo "
+      Install dotfiles
+
+      Options:
+        -h, --help:     Show this help
+        -n, --dry-run:  Show what would have been done, but don't do it.
+    " | sed 's/^ \{6\}//'
+  else
+    echo >&2 "ERROR: Unknown option $1"
+    exit 1
+  fi
+  shift
+done
+
 
 # Change to the script directory.
 cd "$(dirname "$0")"
 
 # Make needed directories.
-find . -mindepth 1 -type d -not -path "*.git*" \
-    | cut -c 3- \
-    | tr '\n' '\0' \
-    | xargs -0 -n 1 mkdir -p
+find "$PWD" -mindepth 1 -type d -not -path '*.git*' \
+    | sed -e "s/^${PWD//\//\\/}/${HOME//\//\\/}/" \
+    | _mkdir -p
 
 # Copy files into place.
-rsync -avh --no-perms \
+_rsync -avh --no-perms \
     --exclude ".git/" \
     --exclude ".DS_Store" \
     --exclude "init" \
     --exclude "install.sh" \
     --exclude "setup.sh" \
-    . ~;
+    "$PWD/" "$HOME/";
