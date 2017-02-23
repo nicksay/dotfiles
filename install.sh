0eee1485db1e7f624851cb4aa355625db556e50d
Alex Nicksay <nicksay@gmail.com>
2017-02-23 01:54:03 -0500

    [install] Script: improve structure, add a confirm prompt, and add a --force option

--- install.sh
+++ install.sh
@@ -1,28 +1,27 @@
 #! /bin/bash
 
-set -e  # stop on error
+set -e  # Stop on error.
+
+# Run from the the script directory.
+cd "$(dirname "$0")"
+
 
 # Global variables.
 DRYRUN=0
+FORCE=0
 
 
-# Functions to handle dry-run versions of commands.
-function _mkdir {
-  # To handle paths piped from find which may have spaces, add the "mkdir"
-  # to each line, echo to the console via tee, then remove the "mkdir" to
-  # be able to pass the paths to mkdir via xargs.
-  sed "s/^/mkdir $@ /" \
-      | tee /dev/tty \
-      | (
-          (( $DRYRUN )) || (
-            sed "s/^mkdir $@ //" \
-                | tr '\n' '\0' \
-                | xargs -0 mkdir $@
-          )
-        )
+function _mkdir() {
+  paths="$(< /dev/stdin)";
+  if (( $DRYRUN )); then
+    echo mkdir $@ $paths
+  else
+    echo "$paths" | tr "\n" "\0" | xargs -0 mkdir $@
+  fi
 }
 
-function _rsync {
+
+function _rsync() {
   if (( $DRYRUN )); then
     echo rsync $@
     rsync -n $@
@@ -32,21 +31,60 @@ function _rsync {
 }
 
 
+function _install() {
+  # Make needed directories.
+  find "$PWD" -mindepth 1 -type d -not -path '*.git*' \
+      | sed -e "s/^${PWD//\//\\/}/${HOME//\//\\/}/" \
+      | _mkdir -p
+  # Copy files into place.
+  _rsync -avh --no-perms \
+      --exclude ".git/" \
+      --exclude ".DS_Store" \
+      --exclude "init" \
+      --exclude "install.sh" \
+      --exclude "setup.sh" \
+      "$PWD/" "$HOME/";
+}
+
+
+function _main() {
+  if (( $DRYRUN )); then
+    echo "Dry run, printing commands...."
+  elif (( $FORCE )); then
+    echo "Installing..."
+  else
+    echo "Installing may overwrite files; use --dry-run to see which ones."
+    read -p "Do you want to continue? (y/N) " -n 1;
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      exit
+    fi
+  fi
+  _install
+}
+
+
+function _help() {
+  echo "
+    Install dotfiles
+
+    Options:
+      -h, --help:     Show this help
+      -n, --dry-run:  Show what would have been done, but don't do it.
+      -f, --force:    Skip verfication before installing.
+  " | sed 's/^ \{4\}//'
+}
+
+
 # Process flags.
 while (( $# > 0 )); do
   if [[ "$1" == "-n" || "$1" == "--dry-run" ]]; then
     DRYRUN=1
-    echo "
-      Dry run! Commands will not be run, just printed.
-    " | sed 's/^ \{6\}//'
+  elif [[ "$1" == "-f" || "$1" == "--force" ]]; then
+    FORCE=1
   elif [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    echo "
-      Install dotfiles
-
-      Options:
-        -h, --help:     Show this help
-        -n, --dry-run:  Show what would have been done, but don't do it.
-    " | sed 's/^ \{6\}//'
+    _help
+    exit
   else
     echo >&2 "ERROR: Unknown option $1"
     exit 1
@@ -55,19 +93,4 @@ while (( $# > 0 )); do
 done
 
 
-# Change to the script directory.
-cd "$(dirname "$0")"
-
-# Make needed directories.
-find "$PWD" -mindepth 1 -type d -not -path '*.git*' \
-    | sed -e "s/^${PWD//\//\\/}/${HOME//\//\\/}/" \
-    | _mkdir -p
-
-# Copy files into place.
-_rsync -avh --no-perms \
-    --exclude ".git/" \
-    --exclude ".DS_Store" \
-    --exclude "init" \
-    --exclude "install.sh" \
-    --exclude "setup.sh" \
-    "$PWD/" "$HOME/";
+_main
