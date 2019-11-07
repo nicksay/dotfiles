9083c741c93685573d9f0805bb0aef9ddde48281
Alex Nicksay <nicksay@gmail.com>
2019-11-07 13:53:17 -0800

    Defaults: configure Spotlight search results

--- init/defaults.sh
+++ init/defaults.sh
@@ -106,6 +106,34 @@ defaults write com.apple.dock springboard-page-duration -float 0.3
 defaults write com.apple.dock show-recents -bool false
 
 
+# Spotlight
+defaults write com.apple.Spotlight orderedItems '(
+    { enabled = 1; name = APPLICATIONS; },
+    { enabled = 1; name = "MENU_CONVERSION"; },
+    { enabled = 1; name = "MENU_EXPRESSION"; },
+    { enabled = 1; name = "MENU_DEFINITION"; },
+    { enabled = 1; name = "SYSTEM_PREFS"; },
+    { enabled = 0; name = "MENU_SPOTLIGHT_SUGGESTIONS"; },
+    { enabled = 0; name = DOCUMENTS; },
+    { enabled = 0; name = DIRECTORIES; },
+    { enabled = 0; name = PRESENTATIONS; },
+    { enabled = 0; name = SPREADSHEETS; },
+    { enabled = 0; name = PDF; },
+    { enabled = 0; name = MESSAGES; },
+    { enabled = 0; name = CONTACT; },
+    { enabled = 0; name = "EVENT_TODO"; },
+    { enabled = 0; name = IMAGES; },
+    { enabled = 0; name = BOOKMARKS; },
+    { enabled = 0; name = MUSIC; },
+    { enabled = 0; name = MOVIES; },
+    { enabled = 0; name = FONTS; },
+    { enabled = 0; name = "MENU_OTHER"; },
+    { enabled = 0; name = SOURCE; }
+)'
+defaults write com.apple.Spotlight showedFTE -bool true
+defaults write com.apple.Spotlight showedLearnMore -bool true
+
+
 # App Store / Software Update
 
 # Enable the automatic update check
