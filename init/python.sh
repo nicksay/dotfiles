363e67e6f8aa3ee70d0ebd1cc75ce05e8cdc5488
Alex Nicksay <nicksay@gmail.com>
2025-05-02 21:40:58 -0700

    Python: Update for Linux installation

--- init/python.sh
+++ init/python.sh
@@ -4,22 +4,30 @@ set -e  # Stop on error.
 cd "$(dirname "$0")" # Run from the the script directory.
 
 
-HOMEBREW_PREFIX="/opt/homebrew"
-PYENV="$HOMEBREW_PREFIX/bin/pyenv"
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  HOMEBREW_PREFIX="/opt/homebrew"
+  PYENV="$HOMEBREW_PREFIX/bin/pyenv"
+  if [[ ! -e "$PYENV" ]]; then
+    echo
+    echo "WARNING: pyenv not found."
+    exit
+  fi
+else
+  PYENV="$HOME/.pyenv/bin/pyenv"
+  if [[ ! -e "$PYENV" ]]; then
+    echo "Installing pyenv..."
+    /bin/bash -c "$(curl -fsSL https://pyenv.run)"
+    echo "pyenv installed."
+  fi
+fi
 PIP="$HOME/.pyenv/shims/pip"
 
 
-echo
-if [[ ! -e "$PYENV" ]]; then
-  echo "WARNING: pyenv not found."
-  exit
-fi
-
-if "$PYENV" version | cut -d ' ' -f 1 | egrep -q '\d+\.\d+\.\d+'; then
+if "$PYENV" version | cut -d ' ' -f 1 | egrep -q '[0-9]+\.[0-9]+\.[0-9]+'; then
   echo "Python already installed."
 else
   echo "Installing python..."
-  python_version=$("$PYENV" install -l | egrep '\s+\d+\.\d+\.\d+$' | tail -1)
+  python_version=$("$PYENV" install -l | egrep '\s+[0-9]+\.[0-9]+\.[0-9]+$' | tail -1)
   "$PYENV" install $python_version
   "$PYENV" global $python_version
   echo "Python installed."
