60f4d76eda9779a6729b7b6c3749722d0574d206
Alex Nicksay <nicksay@gmail.com>
2025-05-02 21:34:17 -0700

    Go: Update for Linux installation

--- init/go.sh
+++ init/go.sh
@@ -4,22 +4,30 @@ set -e  # Stop on error.
 cd "$(dirname "$0")" # Run from the the script directory.
 
 
-HOMEBREW_PREFIX="/opt/homebrew"
-GOENV="$HOMEBREW_PREFIX/bin/goenv"
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  HOMEBREW_PREFIX="/opt/homebrew"
+  GOENV="$HOMEBREW_PREFIX/bin/goenv"
+  if [[ ! -e "$GOENV" ]]; then
+    echo
+    echo "WARNING: goenv not found."
+    exit
+  fi
+else
+  GOENV="$HOME/.goenv/bin/goenv"
+  if [[ ! -e "$GOENV" ]]; then
+    echo "Installing goenv..."
+    git clone https://github.com/go-nv/goenv.git "$HOME/.goenv"
+    echo "goenv installed."
+  fi
+fi
 GO="$HOME/.goenv/shims/go"
 
 
-echo
-if [[ ! -e "$GOENV" ]]; then
-  echo "WARNING: goenv not found."
-  exit
-fi
-
-if "$GOENV" version | cut -d ' ' -f 1 | egrep -q '\d+\.\d+\.\d+'; then
+if "$GOENV" version | cut -d ' ' -f 1 | egrep -q '[0-9]+\.[0-9]+\.[0-9]+'; then
   echo "Go already installed."
 else
   echo "Installing go..."
-  go_version=$("$GOENV" install -l | egrep '\s+\d+\.\d+\.\d+$' | tail -1)
+  go_version=$("$GOENV" install -l | egrep '\s+[0-9]+\.[0-9]+\.[0-9]+$' | tail -1)
   "$GOENV" install -f $go_version
   "$GOENV" global $go_version
   echo "Go installed."
@@ -32,7 +40,6 @@ packages="
   github.com/GoogleChrome/simplehttp2server@latest
 "
 export GOENV_GOPATH_PREFIX="$HOME/.go"
-eval "$("$GOENV" init -)"
 for pkg in $packages; do
   if "$GO" list $pkg &> /dev/null; then
    echo ✓  $pkg
